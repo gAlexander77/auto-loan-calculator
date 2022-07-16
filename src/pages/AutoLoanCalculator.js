8cb71d0330d53f9a2363728415b0efe66d694858
gAlexander77 <87527303+gAlexander77@users.noreply.github.com>
2022-07-15 20:50:53 -0500

    Calculator Inputs

--- src/pages/AutoLoanCalculator.js
+++ src/pages/AutoLoanCalculator.js
@@ -3,7 +3,33 @@ import React from 'react';
 function AutoLoanCalculator() {
     return(
         <div className="AutoLoanCalulator">
-            <h1>Auto Loan Calculator</h1>
+            <div className="Input-Container">  
+                <h1>Auto Loan Calculator</h1>
+                <div className = "Auto-Price">
+                    <p>Auto Price</p>
+                    <input type="number" placeholder="0"/>
+                </div>
+                <div className = "Loan-Term">
+                    <p>Loan Term</p>
+                    <input type="number" placeholder="0"/>
+                </div>
+                <div className = "Interest-Rate">
+                    <p>Interest Rate</p>
+                    <input type="number" placeholder="0"/>
+                </div>
+                <div className = "Down-Payment">
+                    <p>Down Payment</p>
+                    <input type="number" placeholder="0"/>
+                </div>
+                <div className = "Trade-In-Value">
+                    <p>Down Payment</p>
+                    <input type="number" placeholder="0"/>
+                </div>      
+                <div className = "Sales-Tax">
+                    <p>Sales Tax</p>
+                    <input type="number" placeholder="0"/>
+                </div>
+            </div>  
         </div>
     );
 }
